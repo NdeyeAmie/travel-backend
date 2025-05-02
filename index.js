3eca9da1365bcba0579b81b3a9d5cf911aa8e8a6
NdeyeAmie <tndyeamie456@gmail.com>
2025-05-02 00:22:39 +0000

    userModel

--- index.js
+++ index.js
@@ -2,16 +2,26 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db.js");
+const app = express();
+const reservationRoutes = require("./routes/ReservationsRoutes");
+const userRoutes = require("./routes/userRoutes.js")
 
 dotenv.config();
-const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(reservationRoutes);
 
 
-connectDB()
-.then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.DB_PORT}`);
-    });
+
+app.use("/api/users", userRoutes);
+app.use("/api/users/register", userRoutes);
+
+
+
+connectDB().then(() => {
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => {
+    console.log(`Serveur lancé sur le port ${PORT}`);
   });
+})
+
